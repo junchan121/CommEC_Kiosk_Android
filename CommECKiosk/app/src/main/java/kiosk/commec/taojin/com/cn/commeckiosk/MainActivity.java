a07d31ebf1e7c32d0723ce215db5346bacd5b35b
kris.li <kris.li@iisigroup.com>
2016-06-04 22:42:14 +0800

    Added K2.1.1 Community goods attribute dialog view.

--- CommECKiosk/app/src/main/java/kiosk/commec/taojin/com/cn/commeckiosk/MainActivity.java
+++ CommECKiosk/app/src/main/java/kiosk/commec/taojin/com/cn/commeckiosk/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.view_community_goods_attribute);
     }
 
 }
